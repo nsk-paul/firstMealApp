df51784534e2ed829c92a65bc4cadeb3e5231c88
nsk-paul <paulngsk1@gmail.com>
2023-11-04 21:52:59 +0800

    This commit update understanding the spacing of search and list box

--- App.js
+++ App.js
@@ -1,14 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, SafeAreaView, Platform, StatusBar } from 'react-native';
+
+const isAndroid = Platform.OS === 'android';
+
+console.log(StatusBar.currentHeight);
 
 export default function App() {
   return (
-    <View >
-      
-    </View>
+    <>
+    <SafeAreaView style={[styles.container, {marginTop: StatusBar.currentHeight}]}>
+      <Text style={[styles.searchBox]}>
+        search
+      </Text>
+      <Text style={[styles.listBox]}>
+        list
+      </Text>
+    </SafeAreaView>
+     <ExpoStatusBar style ="auto"/>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  
+  container:{
+    flex:1,
+    backgroundColor: "orange"
+  },
+  searchBox: {
+    padding: 16,
+    backgroundColor: "green"
+  },
+  listBox:{
+    flex :1,
+    padding: 16,
+    backgroundColor:"blue",
+    
+    
+  }
 });
